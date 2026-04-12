df31c5292ba0d0076029fdcb6ce91fe76b93f968
Prathamesh Ghormare <prathameshghormare@gmail.com>
2026-04-12 21:29:23 +0530

    add basic return statement parsing rules

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -43,6 +43,38 @@ func TestLetStatements(t *testing.T) {
 
 }
 
+func TestReturnStatements(t *testing.T) {
+	input := ` return 5;
+			return 10;
+			return add(10);`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	
+	checkParserErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+		len(program.Statements))
+	}
+
+	for _, s := range program.Statements {
+		stmt, ok := s.(*ast.ReturnStatement) 
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if stmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+			stmt.TokenLiteral())
+		}
+	}
+}
+
 func testLetStatements(t *testing.T, statement ast.Statement, expected string) bool {
 	if statement.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", statement.TokenLiteral())
